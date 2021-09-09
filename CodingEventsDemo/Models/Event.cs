14e59c38485b389372cb1f1f5a9f509964c148a3
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2021-09-09 10:40:20 -0500

    added basic model

---
+++ CodingEventsDemo/Models/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEventsDemo.Models
+{
+    public class Event
+    {
+        public string Name { get; set; }
+
+        public Event(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
