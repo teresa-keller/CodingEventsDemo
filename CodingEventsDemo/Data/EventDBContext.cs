fea3d08db7dc237c1ac5891f0c038ea3c25cd623
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2021-09-11 17:00:21 -0500

    making progress towards persistance

---
+++ CodingEventsDemo/Data/EventDBContext.cs
@@ -0,0 +1,16 @@
+﻿using CodingEventsDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEventsDemo.Data
+{
+    public class EventDbContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+        public EventDbContext(DbContextOptions<EventDbContext> options) : base(options) { 
+        }
+    }
+}
