c1810a20793a88e2e986b4ac979ca3a4aa563e27
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2021-09-12 20:02:58 -0500

    chapter 19 exercises started

---
+++ CodingEventsDemo/Models/EventCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEventsDemo.Models
+{
+    public class EventCategory
+    {
+        public int Id;
+        public string Name;
+
+        public EventCategory() { }
+        public EventCategory(string name)
+        {
+            Name = name;
+        }
+    }
+}
